582ce8999c6d26faed22bacadcac47fbc5393279
kushan22 <kushansingh22@gmail.com>
2019-12-18 17:19:33 -0500

    Predictions Done.

--- Web-app/app/forms.py
+++ Web-app/app/forms.py
@@ -8,18 +8,7 @@ from wtforms.widgets import TextArea
 import datetime
 from wtforms.fields.html5 import DateTimeField
 
-class CrimeForm(FlaskForm):
-    locationDescription = StringField('Location_description',id='loc_desc',widget=TextArea(),validators=[DataRequired()])
-    arrest = BooleanField('Arrest')
-    domestic = BooleanField('Domestic')
-    beat = StringField('Beat',id='beat',validators=[DataRequired()])
-    district_code = StringField('District Code',id='district_code',validators=[DataRequired()])
-    ward_code = StringField('Ward Code',id='ward',validators=[DataRequired()])
-    community_area = StringField('Community Area',id='community_area',validators=[DataRequired()])
-    fbi_code = StringField('FBI Code',id='fbi_code',validators=[DataRequired()])
-    dateofcrime = DateTimeField('Date and time of Crime',default=datetime.datetime.now(),format="%Y-%m-%d %H:%M:%S")
 
-    submit = SubmitField('Submit')
 
 class Visualization(FlaskForm):
     questionDropdown =  SelectField(
--- Web-app/app/routes.py
+++ Web-app/app/routes.py
@@ -4,29 +4,99 @@ from flask import render_template, request
 
 from datetime import date
 from app import app
-from app.forms import CrimeForm, Visualization
+from app.forms import Visualization
+
+from pyspark.sql import SparkSession
+from pyspark.ml.classification import RandomForestClassificationModel
+from pyspark.ml.feature import IndexToString
+
+
 
 from pyspark.sql import SparkSession
 
+spark = SparkSession\
+        .builder\
+        .appName("HW2")\
+        .getOrCreate()
+
+sc = spark.sparkContext
 
 
 
 bootstrap = Bootstrap(app)
 
+labels = ['THEFT',
+ 'BATTERY',
+ 'CRIMINAL DAMAGE',
+ 'NARCOTICS',
+ 'OTHER OFFENSE',
+ 'ASSAULT',
+ 'BURGLARY',
+ 'MOTOR VEHICLE THEFT',
+ 'ROBBERY',
+ 'DECEPTIVE PRACTICE',
+ 'CRIMINAL TRESPASS',
+ 'PROSTITUTION',
+ 'WEAPONS VIOLATION',
+ 'PUBLIC PEACE VIOLATION',
+ 'OFFENSE INVOLVING CHILDREN',
+ 'CRIM SEXUAL ASSAULT',
+ 'SEX OFFENSE',
+ 'GAMBLING',
+ 'LIQUOR LAW VIOLATION',
+ 'INTERFERENCE WITH PUBLIC OFFICER',
+ 'ARSON',
+ 'HOMICIDE',
+ 'KIDNAPPING',
+ 'INTIMIDATION',
+ 'STALKING',
+ 'OBSCENITY',
+ 'PUBLIC INDECENCY',
+ 'OTHER NARCOTIC VIOLATION',
+ 'NON-CRIMINAL',
+ 'CONCEALED CARRY LICENSE VIOLATION',
+ 'NON - CRIMINAL',
+ 'HUMAN TRAFFICKING',
+ 'RITUALISM',
+ 'NON-CRIMINAL (SUBJECT SPECIFIED)']
 
+
+labelConverter = IndexToString(inputCol="prediction", outputCol="predictionLabel", labels=labels)
 
 @app.route('/',methods=['GET','POST'])
 def index():
-    form = CrimeForm()
-
-    if form.validate_on_submit():
-        return render_template('home.html',form=form,submitted=True)
-
-    return render_template('home.html',form=form,submitted=False)
+    return redirect('/home')
 
 @app.route('/home',methods=['GET','POST'])
 def home():
-    return "Home is here"
+
+    predictionLabels = []
+    labels = []
+    newModel = RandomForestClassificationModel.read().load("./app/static/rf-model")
+    test_df = spark.read.parquet("./app/static/test/*.parquet")
+
+
+    predictions = newModel.transform(test_df)
+    convertedDf = labelConverter.transform(predictions)
+
+    print(convertedDf.printSchema())
+
+    predictionLabel = convertedDf.select("predictionLabel").limit(5).collect()
+
+    label = convertedDf.select("primary_type").limit(5).collect()
+
+    for i in range(len(predictionLabel)):
+        predictionLabels.append(predictionLabel[i]["predictionLabel"])
+
+    for i in range(len(label)):
+        labels.append(label[i]["primary_type"])
+
+    length = len(labels)
+
+
+
+
+    return render_template('home.html',labels=labels,predictionLabels=predictionLabels)
 
 @app.route('/vis',methods=['GET','POST'])
 def vis():
